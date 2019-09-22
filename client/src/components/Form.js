ef8ca05e6c843574d9fdb8839584a1cfe5dacf3b
dokugo <dokugo@users.noreply.github.com>
2019-09-22 20:17:48 +0300

    form post request

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
-  const handleRequest = () => {
+  const [inputData, setInputData] = useState({ newCrawlingRequest: null });
+
+  const handleInputChange = e => {
+    setInputData({ newCrawlingRequest: e.target.value });
+    //console.log(inputData);
+  };
+  /*   async function handleRequest() {
     console.log('request');
+    console.log(inputData);
+    const response = await fetch('http://localhost:9000/api/crawling', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ inputData })
+    });
+    console.log(response);
+  } */
+
+  const handleRequest = () => {
+    fetch('http://localhost:9000/api/crawling', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputData)
+    })
+      .then(res => res.json())
+      .then(data => console.log('Success: ', data));
   };
   return (
     <div className="input-field">
-      <input className="request-input" type="text" />
+      <input
+        onChange={handleInputChange}
+        className="request-input"
+        type="text"
+      />
       <button onClick={handleRequest} className="request-btn">
         Fetch
       </button>
