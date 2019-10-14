8e09df4de147380b21c5a858a27bccd62fe184b4
dokugo <dokugo@users.noreply.github.com>
2019-10-14 04:15:56 +0300

    infinite scrolling

--- client/src/components/VacanciesList.js
+++ client/src/components/VacanciesList.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+/* const style = {
+  height: 30,
+  border: '1px solid green',
+  margin: 6,
+  padding: 8
+}; */
 
 const VacanciesList = props => {
   const { data } = props;
+  const [dataStorage, setDataStorage] = useState(null);
+
+  useEffect(() => {
+    if (data.length <= 8) {
+      setDataStorage({
+        items: data,
+        hasMore: false
+      });
+    } else {
+      const dataChunk = data.slice(0, 8);
+      setDataStorage({
+        items: dataChunk,
+        hasMore: true
+      });
+      console.log('newData');
+    }
+  }, [data]);
+
+  const fetchMoreData = () => {
+    if (dataStorage.items.length >= data.length) {
+      setDataStorage({ items: [...dataStorage.items], hasMore: false });
+      return;
+    }
+    setTimeout(() => {
+      const newDataChunk = data.slice(
+        dataStorage.items.length,
+        dataStorage.items.length + 5
+      );
+      console.log(dataStorage.items.length, dataStorage.items.length + 5);
+      const newData = [...dataStorage.items, ...newDataChunk];
+      setDataStorage({
+        items: newData,
+        hasMore: true
+      });
+    }, 100);
+  };
+
   return (
     <section className="box">
-      {data
-        ? data.map(item => (
+      {dataStorage && dataStorage.items ? (
+        <InfiniteScroll
+          dataLength={dataStorage.items.length}
+          next={fetchMoreData}
+          hasMore={dataStorage.hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>The end</b>
+            </p>
+          }
+        >
+          {dataStorage.items.map((item, index) => (
             <a
               href={item.link}
               key={item.id}
@@ -19,7 +75,7 @@ const VacanciesList = props => {
                     {item.title}
                   </div>
                   <div className="link-item__data link-item__data-date">
-                    {item.date}
+                    {index + 1}
                   </div>
                 </div>
                 <div className="link-item__data link-item__data-employer">
@@ -37,8 +93,9 @@ const VacanciesList = props => {
                 </div>
               </div>
             </a>
-          ))
-        : null}
+          ))}
+        </InfiniteScroll>
+      ) : null}
     </section>
   );
 };
