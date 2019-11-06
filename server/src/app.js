ce3ed3f10adfa95ff637e7f3472fbfe23237eb7b
dokugo <dokugo@users.noreply.github.com>
2019-11-06 23:53:27 +0300

    block duplicate requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,6 @@
+const PromiseController = require('promise-controller');
+const pc = new PromiseController();
+
 const express = require('express');
 const limit = require('express-rate-limit');
 const cors = require('cors');
@@ -7,10 +10,12 @@ const app = express();
 
 const limiter = limit({
   windowMs: 5 * 1000, // 5 second
-  max: 10, // requests per windowMs for each IP
-  message: JSON.stringify(
-    'Too many requests are being made from your IP, please try again after a second or two.'
-  )
+  max: 53, // requests per windowMs for each IP
+  message: JSON.stringify({
+    status: 'error',
+    message:
+      'Too many requests are being made from your IP, please try again after a second or two.'
+  })
 });
 
 app.use(cors());
@@ -22,14 +27,29 @@ console.clear();
 app.get('/api/search/:id', limiter, async (req, res) => {
   try {
     const searchKeywords = req.params.id;
+    console.log(pc.isPending);
 
-    const data = await crawl(searchKeywords);
-    console.log(data);
-
-    if (data && data.length) {
-      res.status(200).send(data);
+    if (pc.isPending) {
+      res.statusMessage = 'Enhance Your Calm';
+      res.status(420).json({
+        status: 'error',
+        message: 'Previous request is processing now.'
+      });
     } else {
-      res.status(200).send(JSON.stringify([{ message: 'Found nothing.' }]));
+      const promise = pc.call(() => crawl(searchKeywords));
+      const data = await promise;
+
+      console.log(data);
+
+      if (data && data.length) {
+        res.status(200).json({
+          status: 'OK',
+          message: 'Data successfully delivered.',
+          data: data
+        });
+      } else {
+        res.status(200).json({ status: 'OK', message: 'Found nothing.' });
+      }
     }
   } catch (error) {
     console.log(error);
