bd7afb8b6e2f656812795b5abb53b46abd82a763
dokugo <dokugo@users.noreply.github.com>
2019-11-08 11:19:04 +0300

    limit parallel crawling funcs num, res handler fix

--- server/src/mutex.js
+++ server/src/mutex.js
@@ -5,6 +5,7 @@ const locksStorage = {};
 const acquire = async id => {
   if (!locksStorage[id]) {
     locksStorage[id] = id;
+
     console.log(`Lock acquired for ${locksStorage[id]}`);
     return 'acquired';
   } else {
@@ -21,7 +22,19 @@ const release = async id => {
 const mutex = async (id, searchKeywords, res) => {
   // console.log('locksStorage: ', locksStorage);
 
+  // limit parallel running puppeteer instances amount
+  if (Object.keys(locksStorage).length > 100) {
+    console.log(locksStorage);
+
+    res.status(503).json({
+      status: 'error',
+      message: 'Server is busy.'
+    });
+    return;
+  }
+
   let lockState;
+
   try {
     lockState = await acquire(id);
 
@@ -29,11 +42,13 @@ const mutex = async (id, searchKeywords, res) => {
       res.statusMessage = 'Enhance Your Calm';
       res.status(420).json({
         status: 'error',
-        message: 'Previous request is processing now.'
+        message: 'Previous request is still processing.'
       });
+      return;
     }
 
     const result = await trySearch(searchKeywords, res);
+    console.log(result);
 
     if (result && result.length) {
       res.status(200).json({
@@ -59,12 +74,14 @@ const mutex = async (id, searchKeywords, res) => {
 const trySearch = async (searchKeywords, res) => {
   try {
     const result = await crawl(searchKeywords);
-    if (!result) {
+
+    // seems unutilized
+    /*     if (!result) {
       res.status(500).json({
         status: 'error',
         message: `Couldn't get data.`
       });
-    }
+    } */
 
     return result;
   } catch (error) {
